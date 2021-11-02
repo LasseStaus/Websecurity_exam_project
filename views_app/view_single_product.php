6a096765d683a117aa2bce403a2c3533217fd190
peter <peetmj@gmail.com>
2021-11-02 14:17:09 +0100

    added css to singleProduct

--- views_app/view_single_product.php
+++ views_app/view_single_product.php
@@ -15,11 +15,22 @@ require('./db/peberString.php');
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/views_app/view_top.php');
 ?>
-
+<link rel="stylesheet" href="/css/singleProduct.css">
 <main>
-    <h1>HEJSA</h1>
-    <h2><?= $product['product_title'] ?></h2>
-    <div class="product">
+<!-- <section id="section1" class="flex wrap column margin-10">
+
+    <div class="flex wrap column">
+    <div><a href="/">Go back</a></div>
+    <div><h1>Single product page</h1></div>
+    </div>
+    <br>
+
+ <div class="flex wrap row">
+    <div class="row">
+        <span>Product title: </span> <strong> <?= $product['product_title'] ?></strong>
+    </div><div></div>
+</div>
+    <div class="product ">
         <div> <strong>USER_ID:</strong> <?= $_SESSION['user_uuid'] ?></div>
         <div> <strong>TS:</strong> <?= $product['product_timestamp'] ?></div>
         <div> <strong>TITLE:</strong> <?= $product['product_title'] ?></div>
@@ -27,6 +38,105 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/views_app/view_top.php');
         <div> <strong>PRICE:</strong> <?= $product['product_price'] ?></div>
         <div> <strong>category:</strong> <?= $product['product_category'] ?></div>
     </div>
+    </section> -->
+
+    <div class = "card-wrapper">
+  <div class = "card">
+    <!-- card left -->
+    <div class = "product-imgs">
+      <div class = "img-display">
+        <div class = "img-showcase">
+          <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_1.jpg" alt = "shoe image">
+          <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_2.jpg" alt = "shoe image">
+          <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_3.jpg" alt = "shoe image">
+          <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_4.jpg" alt = "shoe image">
+        </div>
+      </div>
+      <div class = "img-select">
+        <div class = "img-item">
+          <a href = "#" data-id = "1">
+            <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_1.jpg" alt = "shoe image">
+          </a>
+        </div>
+        <div class = "img-item">
+          <a href = "#" data-id = "2">
+            <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_2.jpg" alt = "shoe image">
+          </a>
+        </div>
+        <div class = "img-item">
+          <a href = "#" data-id = "3">
+            <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_3.jpg" alt = "shoe image">
+          </a>
+        </div>
+        <div class = "img-item">
+          <a href = "#" data-id = "4">
+            <img src = "https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_4.jpg" alt = "shoe image">
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- card right -->
+    <div class = "product-content">
+      <h2 class = "product-title">nike shoes</h2>
+      <a href = "#" class = "product-link">visit nike store</a>
+      <div class = "product-rating">
+        <i class = "fas fa-star"></i>
+        <i class = "fas fa-star"></i>
+        <i class = "fas fa-star"></i>
+        <i class = "fas fa-star"></i>
+        <i class = "fas fa-star-half-alt"></i>
+        <span>4.7(21)</span>
+      </div>
+
+      <div class = "product-price">
+        <p class = "last-price">Old Price: <span>$257.00</span></p>
+        <p class = "new-price">New Price: <span>$<?= $product['product_price'] ?> (5%)</span></p>
+      </div>
+
+      <div class = "product-detail">
+        <h2>about this item: </h2>
+        <p><?= $product['product_description'] ?></p>
+        <ul>
+          <li>Color: <span>Black</span></li>
+          <li>Available: <span>in stock</span></li>
+          <li>Category: <span><?= $product['product_category'] ?></span></li>
+          <li>Shipping Area: <span>All over the world</span></li>
+          <li>Shipping Fee: <span>Free</span></li>
+        </ul>
+      </div>
+
+      <div class = "purchase-info">
+        <input type = "number" min = "0" value = "1">
+        <button type = "button" class = "btn">
+          Add to Cart <i class = "fas fa-shopping-cart"></i>
+        </button>
+        <button type = "button" class = "btn">Compare</button>
+      </div>
+
+      <div class = "social-links">
+        <p>Share At: </p>
+        <a href = "#">
+          <i class = "fab fa-facebook-f"></i>
+        </a>
+        <a href = "#">
+          <i class = "fab fa-twitter"></i>
+        </a>
+        <a href = "#">
+          <i class = "fab fa-instagram"></i>
+        </a>
+        <a href = "#">
+          <i class = "fab fa-whatsapp"></i>
+        </a>
+        <a href = "#">
+          <i class = "fab fa-pinterest"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
     <?php
 
     ?>
@@ -41,6 +151,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/views_app/view_top.php');
 
 </main>
 
+<script src="/js/singleProduct.js"></script>
 
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/views_app/view_bottom.php');
