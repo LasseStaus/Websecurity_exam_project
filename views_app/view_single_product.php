d5201f8784fdb8963d909c0d759664241fe4de20
peter <peetmj@gmail.com>
2021-11-02 15:28:19 +0100

    added out() around everything comming from db

--- views_app/view_single_product.php
+++ views_app/view_single_product.php
@@ -81,24 +81,24 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/views_app/view_top.php');
     </div>
     <!-- card right -->
     <div class = "product-content">
-      <h2 class = "product-title"> <?= $product['product_title'] ?> </h2>
-      <div class="flex wrap column"><p>Created: <span class="small"><?= $product['product_timestamp'] ?></span></p></div>
-      <div class="flex wrap column"><p>By: <span class="small"><?= $product['user_uuid'] ?></span></p></div>
+      <h2 class = "product-title"> <?= out($product['product_title']) ?> </h2>
+      <div class="flex wrap column"><p>Created: <span class="small"><?= out($product['product_timestamp']) ?></span></p></div>
+      <div class="flex wrap column"><p>By: <span class="small"><?= out($product['user_uuid']) ?></span></p></div>
 
 
       <div class = "product-price">
         <p class = "last-price">Old Price: <span>$257.00</span></p>
-        <p class = "new-price">New Price: <span>$<?= $product['product_price'] ?> </span></p>
+        <p class = "new-price">New Price: <span>$<?= out($product['product_price']) ?> </span></p>
       </div>
 
       <div class = "product-detail">
         <h2>about this item: </h2>
-        <p><?= $product['product_description'] ?></p>
+        <p><?= out($product['product_description']) ?></p>
         <ul>
-          <li>Created: <span><?= $product['product_timestamp'] ?></span></li>
-          <li>Title: <span><?= $product['product_title'] ?></span></li>
-          <li>Category: <span><?= $product['product_category'] ?></span></li>
-          <li>Price: <span><?= $product['product_price'] ?></span></li>
+          <li>Created: <span><?= out($product['product_timestamp']) ?></span></li>
+          <li>Title: <span><?= out($product['product_title']) ?></span></li>
+          <li>Category: <span><?= out($product['product_category'] )?></span></li>
+          <li>Price: <span><?= out($product['product_price'] )?></span></li>
           
         </ul>
       </div>
@@ -108,8 +108,8 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/views_app/view_top.php');
       <div class = "purchase-info">
           <h3 class="contact-title">Contact</h3>
       <div class = "product-detail flex wrap column">
-<!--       <span><?= $_SESSION['user_phone'] ?></span>-->    
-<!--       <span><?= $_SESSION['user_email'] ?></span>-->    
+<!--       <span><?= out($_SESSION['user_phone']) ?></span>-->    
+<!--       <span><?= out($_SESSION['user_email']) ?></span>-->    
         <span><i class="fas fa-phone"></i> <a href="tel:xx-xx-xx-xx">xx xx xx xx</a></span>
         <span><i class="fas fa-envelope"></i> <a href="mailto:seller@mail.com">seller@mail.com</a></span>
       </div>
